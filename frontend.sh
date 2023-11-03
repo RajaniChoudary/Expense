2a9a04d63aeb670074d21a745124cc9e4d9e1318
rajani <anilkumar.guntla@gmail.com>
2023-11-03 14:50:11 +0530

    my first commit bybrajani

--- frontend.sh
+++ frontend.sh
@@ -1,6 +1,4 @@
 dnf install nginx -y
-systemctl enable nginx
-systemctl start nginx
 
 cp expense.conf /etc/nginx/default.d/expense.conf
 
@@ -10,4 +8,7 @@ curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zi
 
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+
+systemctl enable nginx
+systemctl start nginx
 
